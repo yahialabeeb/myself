09135121439b850347243d70597f9c0d0d12330d
Yahialabeeb <yahia.labeeb@gmail.com>
2021-06-26 15:52:09 +0300

    lab05b

--- js/app.js
+++ js/app.js
@@ -1,70 +1,116 @@
 'use strict';
 
-var z = 0
+let z = 0
 
 let name = prompt("let us know who are you")
-alert("Nice to meet you " + name + ' lets start playing')
+alert("Nice to meet you " + name + ' let us start playing')
+
 function Qname() {
+    let a1 = true
+    while (a1) {
+        let x1 = prompt("My last name labib?", "(yes/no) or (y/n)")
 
-
-    var x1 = prompt("My last name labib?", "(yes/no)")
-
-    if (x1.toLowerCase() == "yes") {
-        alert("Right my last name is labib")
-        z++
+        if (x1.toLowerCase() == "yes" || x1.toLowerCase() == "y") {
+            alert("Right my last name is labib")
+            z++
+            a1 = false
+        }
+        else if (x1.toLowerCase() == "no" || x1.toLowerCase() == "n") {
+            alert("I am disapointed")
+            a1 = false
+        }
+        else {
+            alert("Please enter yes or no")
+        }
     }
-    else { alert("I am disapointed") }
 }
 Qname();
 function Qage() {
+    let a1 = true
+    while (a1) {
 
 
-    var x2 = prompt("Did I born in 1996?", "(yes/no)")
-    if (x2.toLowerCase() == "yes") {
-        alert("Right I am 25 years old")
-        z++
+        let x2 = prompt("Was I born in 1996?", "(yes/no) or (y/n)")
+        if (x2.toLowerCase() == "yes" || x2.toLowerCase() == "y") {
+            alert("Right I am 25 years old")
+            z++
+            a1 = false
+        }
+        else if (x2.toLowerCase() == "no" || x2.toLowerCase() == "n") {
+            alert("I am disapointed")
+            a1 = false
+        }
+        else {
+            alert("please enter yes or no")
+        }
     }
-    else { alert("I am disapointed") }
 }
 Qage();
 
 function Qstudy() {
+    let a1 = true
+    while (a1) {
+        let x3 = prompt("Am I a civil engineer?", "(yes/no) or (y/n)")
 
-
-    var x3 = prompt("Am I a civil engineer?", "(yes/no)")
-
-    if (x3.toLowerCase() == "no") {
-        alert("Right I am a mechanical engineeer")
-        z++
+        if (x3.toLowerCase() == "no" || x3.toLowerCase() == "n") {
+            alert("Right I am a mechanical engineeer")
+            z++
+            a1 = false
+        }
+        else if (x3.toLowerCase() == "yes" || x3.toLowerCase() == "y") {
+            alert("I am disapointed")
+            a1 = false
+        }
+        else {
+            alert("please enter yes or no")
+        }
     }
-    else { alert("I am disapointed") }
 }
 Qstudy();
 
 function Qinterest() {
-
-    var x4 = prompt("Am I intersted in programing?", "(yes/no)")
-    if (x4.toLowerCase() == "yes") {
-        alert("Right I am very intersted in programing")
-        z++
+    let a1 = true
+    while (a1) {
+        let x4 = prompt("Am I intersted in programing?", "(yes/no) or (y/n)")
+        if (x4.toLowerCase() == "yes" || x4.toLowerCase() == "y") {
+            alert("Right I am very intersted in programing")
+            z++
+            a1 = false
+        }
+        else if (x4.toLowerCase() == "no" || x4.toLowerCase() == "n") {
+            alert("I am disapointed")
+            a1 = false
+        }
+        else {
+            alert("Please enter yes or no")
+        }
     }
-    else { alert("I am disapointed") }
 }
 Qinterest();
 
 function Qgraduate() {
-    var x5 = prompt("Did i graduate from JU?", "(yes/no)")
-    if (x5.toLowerCase() == "yes") {
-        alert("Right I stuided in JU")
-        z++
+    let a1 = true
+    while (a1) {
+        let x5 = prompt("Did I graduate from JU?", "(yes/no) or (y/n)")
+        if (x5.toLowerCase() == "yes" || x5.toLowerCase() == "y") {
+            alert("Right I stuided in JU")
+            z++
+            a1 = false
+        }
+        else if (x5.toLowerCase() == "no" || x5.toLowerCase() == "n") {
+            alert("I am disapointed")
+            a1 = false
+        }
+        else {
+            alert("Please enter yes or no")
+        }
     }
-    else { alert("I am disapointed") }
 }
 Qgraduate();
 function Qold() {
 
 
-    var D1 = prompt("how old am I", "please inter a number")
+    let D1 = prompt("how old am I", "please inter a number")
     for (let i = 0; i < 3; i++) {
         if (D1 == 25) {
             z++
@@ -73,11 +119,11 @@ function Qold() {
         }
         else if (21 <= D1 && D1 <= 29) {
             alert("you are approaching")
-            var D1 = prompt("how old am I", "please inter a number")
+            D1 = prompt("how old am I", "please inter a number")
         }
         else {
             alert("You are far away")
-            var D1 = prompt("How old am I", "please inter a number")
+            D1 = prompt("How old am I", "please inter a number")
         }
     }
     if (D1 != 25) {
@@ -86,52 +132,10 @@ function Qold() {
 }
 Qold();
 
-/* var D2 = prompt("when did Jordan become independent?", "year")
-for (let i = 0; i < 3; i++){
-if (D2 == 1946){
-    w++ 
-    alert("Good Job !!") 
-    break;
-}
-else if (1940 <= D1 && D1 <= 1950) 
-{
-alert("you are approaching")
-var D2 = prompt("when did Jordan become independent?", "year")
-}
-else {
-    alert("You are far away")
-    var D2 = prompt("when did Jordan become independent?", "year")
-}
-}
-if (D2 != 1946){
-    alert("you use all your attempt foucsing in other questions")
-}
-var D3 = prompt("how many brothers do i have?", "1,2,3,4,5..")
-for (let i = 0; i < 3; i++){
-   if (D2 == 4){
-       w++ 
-       alert("Good Job !!") 
-       break;
-   }
-   else if (3 <= D1 && D1 <= 5) 
-   {
-   alert("you are approaching")
-   var D3 = prompt("how many brothers do i have?", "1,2,3,4,5..")
-   }
-   else {
-       alert("You are far away")
-       var D3 = prompt("how many brothers do i have?", "1,2,3,4,5..")
-   }
-}
-   if (D3 != 4){
-       alert("you use all your attempt foucsing in other questions")
-   } */
-
 function Qguess() {
 
-
     let c1 = []
-    alert("Now we will play anoter game, you should guess three numbers from 1 to 10")
+    alert("Now, we will play another game, you should guess three numbers from 1 to 10")
     for (let i = 0; i < 6; i++) {
         for (let q = 1; q < 4; q++) {
             c1.push(prompt("guess number " + q))
@@ -143,7 +147,7 @@ function Qguess() {
             break;
         }
         else if (i == 5) {
-            alert('wrong answer, the right answer is 1 2 3 without consdiring the order')
+            alert('Wrong answer, the right answer is 1 2 3 without consdiring the order')
         }
         else {
             c1 = []
@@ -154,13 +158,12 @@ function Qguess() {
 }
 Qguess();
 
-// console.log(c1)
 function scoor() {
 
 
     switch (z) {
         case 7:
-            alert(name + ' you answered all the questions right', "you know me well")
+            alert(name + ' you answered all the questions right, you know me well')
             break;
 
         case 6:
